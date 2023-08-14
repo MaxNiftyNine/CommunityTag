b3a7f57eb2507f2f0ede985e3821ebb0b9afc3eb
STEEZO983 <141975015+STEEZO983@users.noreply.github.com>
2023-08-13 23:15:26 -0400

    More fixes
    More Bug Fixes

--- Assets/Scripts/kick.cs
+++ Assets/Scripts/kick.cs
@@ -1,11 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Photon.Pun;
 
 public class kick : MonoBehaviour
 {
+    public PhotonView ptView;
+
     void OnTriggerEnter(Collider other)
     {
+        if (ptView.IsMine)
+        {
+            return;
+        }
+        else
         {
             Application.Quit();
         }
