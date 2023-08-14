f7be9f91f3ddbe7c0d61d45747b4432ec481ba42
STEEZO983 <141975015+STEEZO983@users.noreply.github.com>
2023-08-13 22:01:04 -0400

    added scripts, sounds, and a physics cube
    i added alot of scripts sounds and 1 physics cube

---
+++ Assets/Scripts/kick.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class kick : MonoBehaviour
+{
+    void OnTriggerEnter(Collider other)
+    {
+        {
+            Application.Quit();
+        }
+    }
+}
